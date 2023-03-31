c97c60be5c8ecf6e2e3e79248c3dd4496f9cbe4c
lixelamos <amosbett99@gmail.com>
2023-04-01 00:34:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+for (let i = 0; i < arr.length; i++) {
+for (let j = i + 1; j < arr.length; j++) {
+if (arr[i] + arr[j] === target) {
+return true;
+}
+}
+}
+return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
+
+
+
+ function hasTargetSum(arr, target) {
+const complementMap = {};
+for (let i = 0; i < arr.length; i++) {
+const complement = target - arr[i];
+if (complementMap[complement]) {
+return true;
+}
+complementMap[arr[i]] = true;
+}
+return false;
+}
+
 
 /* 
   Add your pseudocode here
+  Create a function called hasTargetSum that takes an array of integers and a target integer as arguments.
+Loop through the array of integers.
+For each element in the array, check if there exists another element in the array such that the sum of the two elements is equal to the target integer.
+If such a pair exists, return true.
+If the loop completes and no pair is found, return false.
+11
 */
 
 /*
   Add written explanation of your solution here
+  function hasTargetSum(arr, target) {
+let seen = new Set();
+for (let i = 0; i < arr.length; i++) {
+let difference = target - arr[i];
+if (seen.has(difference)) {
+return true;
+}
+seen.add(arr[i]);
+}
+return false;
+}
 */
 
 // You can run `node index.js` to view these console logs
